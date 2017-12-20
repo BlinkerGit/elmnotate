3e3d27f3fbaa0e33b9abf9b081ad03389a2cf5ef
Solomon White <rubysolo@gmail.com>
2017-12-19 17:37:35 -0700

    make render async so that shapes are drawn after elm renders the canvas

--- ports.js
+++ ports.js
@@ -8,19 +8,37 @@ let container = document.getElementById('container')
 // and keep a reference for communicating with the app
 let annotate = Elm.Main.embed(container)
 
+
+function withCanvasContext(cb) {
+    const canvas = document.getElementById('annotate-canvas');
+    if (canvas) {
+        const ctx = canvas.getContext('2d');
+        cb(canvas, ctx);
+    } else {
+        console.log("unable to get reference to canvas!");
+    }
+}
+
 // draw graphics on the canvas
 annotate.ports.render.subscribe(function(graphics) {
-    clearCanvas();
-    highlight(graphics.highlight);
-    graphics.points.forEach(point => {
-        drawPoint(point.x, point.y);
-    });
-    graphics.lines.forEach(line => {
-        drawLine(line.start, line.end);
-    });
-    graphics.anchors.forEach(point => {
-        drawAnchor(point.x, point.y);
-    });
+    const doRender = function() {
+        clearCanvas();
+        if (graphics.highlight.length > 0) {
+            highlight(graphics.highlight);
+        }
+        graphics.points.forEach(point => {
+            drawPoint(point.x, point.y);
+        });
+        graphics.lines.forEach(line => {
+            drawLine(line.start, line.end);
+        });
+        graphics.anchors.forEach(point => {
+            drawAnchor(point.x, point.y);
+        });
+    };
+
+    // make render async
+    setTimeout(doRender, 100);
 });
 
 // discover the image size and report back to elm
@@ -58,68 +76,60 @@ function getImageSize(url, cb) {
     image.src = url;
 }
 
+const tau = 2 * Math.PI;
+
 function clearCanvas() {
-    const canvas = document.getElementById("annotate-canvas");
-    if (canvas) {
-        const ctx = canvas.getContext('2d');
+    withCanvasContext(function(canvas, ctx) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         // HACK: draw something transparent to trigger canvas repaint
         ctx.globalAlpha = 0.01;
         ctx.beginPath();
         ctx.arc(0, 0, 10, 0, tau, false);
+        ctx.closePath();
         ctx.fillStyle = 'red';
         ctx.fill();
 
         ctx.globalAlpha = 1.0;
-    }
+    });
 }
 
-const tau = 2 * Math.PI;
 
 function drawPoint(x, y) {
-    const canvas = document.getElementById("annotate-canvas");
-    if (canvas) {
-        const ctx = canvas.getContext('2d');
+    withCanvasContext(function(canvas, ctx) {
         ctx.beginPath();
         ctx.arc(x-1, y-1, 2, 0, tau, false);
+        ctx.closePath();
         ctx.strokeStyle = 'rgb(50,205,50)';
-        //ctx.closePath();
         ctx.stroke();
-    }
+    });
 }
 
 function drawAnchor(x, y) {
-    const canvas = document.getElementById("annotate-canvas");
-    if (canvas) {
-        const ctx = canvas.getContext('2d');
+    withCanvasContext(function(canvas, ctx) {
         ctx.beginPath();
         ctx.arc(x-1, y-1, 2, 0, tau, false);
+        ctx.closePath();
         ctx.strokeStyle = 'red';
         ctx.fillStyle = 'white';
-        //ctx.closePath();
         ctx.stroke();
         ctx.fill();
-    }
+    });
 }
 
 function drawLine(start, end) {
-    const canvas = document.getElementById("annotate-canvas");
-    if (canvas) {
-        const ctx = canvas.getContext('2d');
+    withCanvasContext(function(canvas, ctx) {
         ctx.beginPath();
         ctx.moveTo(start.x, start.y);
         ctx.lineTo(end.x, end.y);
+        ctx.closePath();
         ctx.strokeStyle = 'red';
-        //ctx.closePath();
         ctx.stroke();
-    }
+    });
 }
 
 function highlight(lines) {
-    const canvas = document.getElementById("annotate-canvas");
-    if (canvas) {
-        const ctx = canvas.getContext('2d');
+    withCanvasContext(function(canvas, ctx) {
         ctx.beginPath();
 
         lines.forEach(function (line, index) {
@@ -127,10 +137,10 @@ function highlight(lines) {
                 ctx.moveTo(line.start.x, line.start.y);
             }
             ctx.lineTo(line.end.x, line.end.y);
-        })
+        });
 
         ctx.closePath();
         ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
         ctx.fill();
-    }
+    });
 }
